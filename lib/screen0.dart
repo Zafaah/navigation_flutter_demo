56609a2a3c7691147eaadf73af69e78c394cc8cf
Hafsa Abdukadir <hafzaabdulkhadir@gmail.com>
2023-12-31 16:00:26 +0300

    screen 0

--- lib/screen0.dart
+++ lib/screen0.dart
@@ -5,41 +5,13 @@ import 'package:navigation_flutter_demo/screen2.dart';
 class Screen0 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: Text('Screen 0'),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-              ),
-              child: Text('Go To Screen 1'),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder:(context){
-                  return Screen1();
-                }));
-              },
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-              ),
-              child: Text('Go To Screen 2'),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder:(context){
-                  return Screen2();
-                }));
-                //Navigate to Screen 2
-              },
-            ),
-          ],
-        ),
-      ),
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/':(context)=>Screen0(),
+        '/frist':(context)=> Screen1(),
+        '/second': (context)=>Screen2(),
+      },
     );
   }
 }
